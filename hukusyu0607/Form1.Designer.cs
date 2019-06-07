165e2c29bf74d086e5114d9ced445c724354e426
ozeki <hhdfgg@users.noreply.github.com>
2019-06-07 15:18:49 +0900

    たくさん追加

--- hukusyu0607/Form1.Designer.cs
+++ hukusyu0607/Form1.Designer.cs
@@ -31,12 +31,16 @@
             this.components = new System.ComponentModel.Container();
             this.label1 = new System.Windows.Forms.Label();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.counter = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.ForeColor = System.Drawing.Color.Blue;
             this.label1.Location = new System.Drawing.Point(125, 124);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(67, 24);
@@ -49,11 +53,48 @@
             this.timer1.Enabled = true;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.ForeColor = System.Drawing.Color.Blue;
+            this.label2.Location = new System.Drawing.Point(429, 204);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label3.ForeColor = System.Drawing.Color.Blue;
+            this.label3.Location = new System.Drawing.Point(289, 109);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 24);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "label3";
+            // 
+            // counter
+            // 
+            this.counter.AutoSize = true;
+            this.counter.Font = new System.Drawing.Font("MS UI Gothic", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.counter.Location = new System.Drawing.Point(0, 0);
+            this.counter.Name = "counter";
+            this.counter.Size = new System.Drawing.Size(108, 29);
+            this.counter.TabIndex = 3;
+            this.counter.Text = "counter";
+            this.counter.Click += new System.EventHandler(this.counter_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(615, 365);
+            this.Controls.Add(this.counter);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -66,6 +107,9 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label counter;
     }
 }
 
--- hukusyu0607/Form1.cs
+++ hukusyu0607/Form1.cs
@@ -12,21 +12,64 @@ namespace hukusyu0607
 {
     public partial class Form1 : Form
     {
-        int vx = -10;
-        int vy = -10;
+        // 疑似乱数
+        // ランダムのシード(種)を指定して初期化したら、
+        // それを使い続ける。
+        private static Random rand = new Random();
+        int vx = rand.Next(-10,11);
+        int vy = rand.Next(-10,11);
+        int vx2 = rand.Next(-10,11);
+        int vy2 = rand.Next(-10,11);
+        int vx3 = rand.Next(-10, 11);
+        int vy3 = rand.Next(-10, 11);
+
+        int time = 500;
 
         public Form1()
         {
             InitializeComponent();
+            label1.Left = rand.Next(0, ClientSize.Width);
+            label1.Top = rand.Next(0, ClientSize.Height);
+            label2.Left = rand.Next(0, ClientSize.Width);
+            label2.Top = rand.Next(0, ClientSize.Height);
+            label3.Left = rand.Next(0, ClientSize.Width);
+            label3.Top = rand.Next(0, ClientSize.Height);
+
+            //以下　授業前
+            /*
+            Random lx = new Random();
+            Random ly = new Random();
+            label1.Left = lx.Next(0, ClientSize.Width);
+            label1.Top = ly.Next(0, ClientSize.Height);
+
+            Random ho = new Random();
+            int hou = ho.Next(1, 5);
+            switch (hou)
+            {
+                case 1:
+                    vx = -10;
+                    vy = -10;
+                    break;
+                case 2:
+                    vx = 10;
+                    vy = -10;
+                    break;
+                case 3:
+                    vx = -10;
+                    vy = 10;
+                    break;
+                case 4:
+                    vx = 10;
+                    vy = 10;
+                    break;
+            }*/
+            //以上　授業前
 
         }
 
         private void label1_Click(object sender, EventArgs e)
         {
-            //Random lx = new Random();
-            //Random ly = new Random();
-            //label1.Left = lx(0, ClientSize.Width);
-            //label1.Left = ly(0, ClientSize.Height);
+
         }
 
         private void timer1_Tick(object sender, EventArgs e)
@@ -36,46 +79,207 @@ namespace hukusyu0607
 
             if (label1.Left + label1.Width / 2 < 0)
             {
-                Math.Abs(vx = -vx);
+                //Math.Abs(vx = -vx);
+                vx = Math.Abs(vx);
                 //vx = -vx;
             }
             if (label1.Left + label1.Width / 2 > ClientSize.Width)
             {
-                Math.Abs (vx = -vx);
+                vx = -Math.Abs(vx);
+                //Math.Abs (vx = -vx);
                 //vx = -vx;
             }
             if (label1.Top + label1.Height / 2 < 0)
             {
-                Math.Abs(vy = -vy);
+                vy = Math.Abs(vy);
+                //Math.Abs(vy = -vy);
                 //vy = -vy;
             }
             if (label1.Top + label1.Height / 2 > ClientSize.Height)
             {
-                Math.Abs(vy = -vy);
+                vy = -Math.Abs(vy);
+                //Math.Abs(vy = -vy);
                 //vy = -vy;
             }
-            int mx = MousePosition.X;//Form1の大きさの判定じゃない
-            int my = MousePosition.Y;
-            if (label1.Left<mx&&label1.Right>mx&&label1.Top<my&&label1.Bottom>my)
+            //int mx = MousePosition.X;//Form1の大きさの判定じゃない
+            //int my = MousePosition.Y;
+            Point p = PointToClient(MousePosition);
+            if (label3.Left<p.X&&label3.Right>p.X&&label3.Top<p.Y&&label3.Bottom>p.Y)
+            {
+                //label3.ForeColor = Color.Yellow;
+                //timer1.Enabled = false;
+                label3.Visible = false;
+            }
+            if (label2.Left<p.X&&label2.Right>p.X&&label2.Top<p.Y&&label2.Bottom>p.Y)
+            {
+                label2.Visible = false;
+            }
+            if (label1.Left < p.X && label1.Right > p.X && label1.Top < p.Y && label1.Bottom > p.Y)
+            {
+                label1.Visible = false;
+            }
+            if (label1.Visible==false&&label2.Visible==false&&label3.Visible==false)
             {
                 timer1.Enabled = false;
             }
-            if (vx==-10&&vy==-10)
+            
+            if (vx < 0 && vy < 0)
             {
                 label1.Text = ("↖");
             }
-            if (vx == -10 && vy == 10)
+            if (vx < 0 && vy >0)
             {
                 label1.Text = ("↙");
             }
-            if (vx == 10 && vy == -10)
+            if (vx > 0 && vy < 0)
             {
                 label1.Text = ("↗");
             }
-            if (vx == 10 && vy == 10)
+            if (vx > 0 && vy > 0)
             {
                 label1.Text = ("↘");
             }
+            if (vx == 0 && vy < 0)
+            {
+                label1.Text=("↑");
+            }
+            if (vx == 0 && vy > 0)
+            {
+                label1.Text=("↓");
+            }
+            if (vx < 0 && vy == 0)
+            {
+                label1.Text=("←");
+            }
+            if (vx > 0 && vy == 0)
+            {
+                label1.Text=("→");
+            }
+
+            //以下 ラベルのあれ　コピペ
+            label2.Left += vx2;
+            label2.Top += vy2;
+            if (label2.Left + label2.Width / 2 < 0)
+            {
+                vx2 = Math.Abs(vx2);
+            }
+            if (label2.Left + label2.Width / 2 > ClientSize.Width)
+            {
+                vx2 = -Math.Abs(vx2);
+            }
+            if (label2.Top + label2.Height / 2 < 0)
+            {
+                vy2 = Math.Abs(vy2);
+            }
+            if (label2.Top + label2.Height / 2 > ClientSize.Height)
+            {
+                vy2 = -Math.Abs(vy2);
+            }
+            if (vx2 < 0 && vy2 < 0)
+            {
+                label2.Text = ("↖");
+            }
+            if (vx2 < 0 && vy2 > 0)
+            {
+                label2.Text = ("↙");
+            }
+            if (vx2 > 0 && vy2 < 0)
+            {
+                label2.Text = ("↗");
+            }
+            if (vx2 > 0 && vy2 > 0)
+            {
+                label2.Text = ("↘");
+            }
+            if (vx2 == 0 && vy2 < 0)
+            {
+                label2.Text = ("↑");
+            }
+            if (vx2 == 0 && vy2 > 0)
+            {
+                label2.Text = ("↓");
+            }
+            if (vx2 < 0 && vy2 == 0)
+            {
+                label2.Text = ("←");
+            }
+            if (vx2 > 0 && vy2 == 0)
+            {
+                label2.Text = ("→");
+            }
+
+            //ぶつかったら
+            /*
+            if (label1.Left<label2.Right&&label1.Top<label2.Bottom&&label1.Right>label2.Left&&label1.Bottom>label2.Top)
+            {
+                label1.Visible = false;
+                label2.Visible = false;
+            }*/
+
+            //ラベル3
+            label3.Left += vx3;
+            label3.Top += vy3;
+            if (label3.Left + label3.Width / 2 < 0)
+            {
+                vx3 = Math.Abs(vx3);
+            }
+            if (label3.Left + label3.Width / 2 > ClientSize.Width)
+            {
+                vx3 = -Math.Abs(vx3);
+            }
+            if (label3.Top + label3.Height / 2 < 0)
+            {
+                vy3 = Math.Abs(vy3);
+            }
+            if (label3.Top + label3.Height / 2 > ClientSize.Height)
+            {
+                vy3 = -Math.Abs(vy3);
+            }
+            if (vx3 < 0 && vy3 < 0)
+            {
+                label3.Text = ("↖");
+            }
+            if (vx3 < 0 && vy3 > 0)
+            {
+                label3.Text = ("↙");
+            }
+            if (vx3 > 0 && vy3 < 0)
+            {
+                label3.Text = ("↗");
+            }
+            if (vx3 > 0 && vy3 > 0)
+            {
+                label3.Text = ("↘");
+            }
+            if (vx3 == 0 && vy3 < 0)
+            {
+                label3.Text = ("↑");
+            }
+            if (vx3 == 0 && vy3 > 0)
+            {
+                label3.Text = ("↓");
+            }
+            if (vx3 < 0 && vy3 == 0)
+            {
+                label3.Text = ("←");
+            }
+            if (vx3 > 0 && vy3 == 0)
+            {
+                label3.Text = ("→");
+            }
+
+            time--;
+            counter.Text=("time: "+time);
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void counter_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
